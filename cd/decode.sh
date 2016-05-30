32c43da6da43c02155cc62982db13446c1d73b36
Alberto Irurueta Carro <albertoirurueta@gmail.com>
2016-05-31 00:26:46 +0200

    Updated decoding script

--- cd/decode.sh
+++ cd/decode.sh
@@ -1,3 +1,8 @@
 openssl aes-256-cbc -K $encrypted_99e0f175f049_key -iv $encrypted_99e0f175f049_iv -in apache-mail.properties.enc -out apache-mail.properties -d
+echo "Apache mail decoded"
+
 openssl aes-256-cbc -K $encrypted_99e0f175f049_key -iv $encrypted_99e0f175f049_iv -in aws-mail.properties.enc -out aws-mail.properties -d
+echo "AWS mail decoded"
+
 openssl aes-256-cbc -K $encrypted_99e0f175f049_key -iv $encrypted_99e0f175f049_iv -in java-mail.properties.enc -out java-mail.properties -d
+echo "Java mail decoded"
