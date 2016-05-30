75bf0cb1c87261dbb201fd5f47af2e8ccf4bf647
Alberto Irurueta Carro <albertoirurueta@gmail.com>
2016-05-31 00:43:02 +0200

    Updated travis configuration

--- cd/before-deploy.sh
+++ cd/before-deploy.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 if [ "$TRAVIS_BRANCH" = 'master' ] && [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
-    gpg --fast-import config/codesigning.asc
+    gpg --fast-import codesigning.asc
 fi
--- cd/decode.sh
+++ cd/decode.sh
@@ -1,5 +1,3 @@
 openssl aes-256-cbc -K $encrypted_99e0f175f049_key -iv $encrypted_99e0f175f049_iv -in config.tar.gz.enc -out config.tar.gz -d
 
-cp config/apache-mail.properties ./apache-mail.properties
-cp config/aws-mail.properties ./aws-mail.properties
-cp config/java-mail.properties ./java-mail.properties
+tar -zxvf config.tar.gz
