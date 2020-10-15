063d49f6f6c40b6a77a4e44f73b9768686fb157f
Alberto Irurueta <alberto@irurueta.com>
2020-10-16 00:21:44 +0200

    Updated deploy configuration

--- cd/before-deploy.sh
+++ cd/before-deploy.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 if [ "$TRAVIS_BRANCH" = 'master' ] && [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
-    gpg --fast-import codesigning.asc
+    openssl aes-256-cbc -K $encrypted_81c98acad902_key -iv $encrypted_81c98acad902_iv -in cd/codesigning.asc.enc -out cd/codesigning.asc -d
+    gpg --fast-import cd/codesigning.asc
 fi
--- cd/decode.sh
+++ cd/decode.sh
@@ -1,3 +1,3 @@
-openssl aes-256-cbc -K $encrypted_99e0f175f049_key -iv $encrypted_99e0f175f049_iv -in config.tar.gz.enc -out config.tar.gz -d
+openssl aes-256-cbc -K $encrypted_52c610782802_key -iv $encrypted_52c610782802_iv -in config.tar.gz.enc -out config.tar.gz -d
 
 tar -zxvf config.tar.gz
